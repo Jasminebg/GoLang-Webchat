5b7013dd60fc0e6c97b16c4d16ba3585e708a6ff
Jasmine Baig <jasminebaig9@gmail.com>
2021-06-29 20:05:55 -0400

    fixing deployment

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,7 +9,7 @@ import (
 	"github.com/Jasminebg/GoLang-Webchat/backend/pkg/websocket"
 )
 
-var addr = flag.String("addr", ":8080", "http server address")
+// var addr = flag.String("addr", ":8080", "http server address")
 
 func main() {
 	// chatServer := ChatServer{make([]websocket.MessageData, 0)}
@@ -19,7 +18,6 @@ func main() {
 	port := os.Getenv("PORT")
 	pool := websocket.NewPool()
 	go pool.Start()
-	portAddr := flag.String("addr", ":"+port, "http server address")
 
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		websocket.ServeWs(pool, w, r)
@@ -30,9 +28,9 @@ func main() {
 	fs := http.FileServer(http.Dir("./build"))
 	http.Handle("/", fs)
 	if !(port == "") {
-		log.Fatal(http.ListenAndServe(*portAddr, nil))
+		log.Fatal(http.ListenAndServe(":"+port, nil))
 	} else {
-		log.Fatal(http.ListenAndServe(*addr, nil))
+		log.Fatal(http.ListenAndServe(":"+"8080", nil))
 	}
 	// log.Fatal(http.ListenAndServe(*addr, nil))
 }
