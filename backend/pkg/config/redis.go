e51f39967173a2e2c23493fca99e96cb22398ba7
Jasmineb <33909526+Jasminebg@users.noreply.github.com>
2021-09-01 21:44:05 -0400

    a

--- backend/pkg/config/redis.go
+++ backend/pkg/config/redis.go
@@ -1,11 +1,16 @@
 package config
 
-import "github.com/go-redis/redis/v8"
+import (
+	"os"
+
+	"github.com/go-redis/redis/v8"
+)
 
 var Redis *redis.Client
 
 func CreateRedisClient() {
-	opt, err := redis.ParseURL("redis://localhost:6379/0")
+	// opt, err := redis.ParseURL("redis://localhost:6379/0")
+	opt, err := redis.ParseURL(os.Getenv("REDISCLOUD_URL"))
 	if err != nil {
 		panic(err)
 	}
